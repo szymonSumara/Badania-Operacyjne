58a9deaead1337f56dddace4af7fd516467535fa
studzik21 <pstudzik@student.agh.edu.pl>
2022-05-30 17:04:48 +0200

    save map to file

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 #map parameters
 petrol_prices=(1, 100)
 map_size=2000
-petrol_stations_number=300
+petrol_stations_number=200
 
 tank_capacity = 600
-start_population_size=500
+start_population_size=50
 
-a = 0
+a = 1
 b = 1
 
 #Algorithm parameters
-number_of_epochs = 15
-number_of_crossing = 100
-number_of_fuel_mutation = 10000
+number_of_epochs = 30
+number_of_crossing = 500
+number_of_fuel_mutation = 100
 number_of_add_neighbour_mutation = 100
 number_of_skip_neighbour_mutation = 100
 
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+import pickle
 
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from Map.graph import Graph
 from Map.utils import create_random_map, get_first_and_last_station
 from Genetic.core import genetic_algorithm
 
@@ -19,8 +20,12 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
     petrol_stations_map = create_random_map()
-    nx.write_adjlist( petrol_stations_map.graph, "graph.txt")
+    # nx.write_weighted_edgelist( petrol_stations_map.graph, "graph.txt")
+    with open('graph.txt', 'wb') as config_dictionary_file:
+        pickle.dump(petrol_stations_map, config_dictionary_file)
 
+    petrol_stations_map = pickle.load(open('graph.txt', 'rb'))
+    petrol_stations_map.show()
     start_station, end_station = get_first_and_last_station(petrol_stations_map.graph.nodes)
     genetic_algorithm(petrol_stations_map, start_station, end_station)
 
